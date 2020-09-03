96c65fbf39bdfe1f886192dea1ce4291148b70f0
Mateus <mateusnobref@gmail.com>
2020-09-03 09:52:28 -0300

    Initial Commit

---
+++ EscoteiroEspacial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EscoteiroEspacial
+//
+//  Created by Mateus Nobre on 03/09/20.
+//  Copyright © 2020 Mateus Nobre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
