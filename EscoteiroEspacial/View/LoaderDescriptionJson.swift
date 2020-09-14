9fe2ff0b68a41130242eb6807da59f38aa0ac7bb
Paulo Rubem <rubem.oliveira27@hotmail.com>
2020-09-14 10:40:25 -0300

    Adicionado o json de descricao

---
+++ EscoteiroEspacial/View/LoaderDescriptionJson.swift
@@ -0,0 +1,9 @@
+//
+//  LoaderDescriptionJson.swift
+//  EscoteiroEspacial
+//
+//  Created by Paulo Uchôa on 14/09/20.
+//  Copyright © 2020 Mateus Nobre. All rights reserved.
+//
+
+import Foundation
