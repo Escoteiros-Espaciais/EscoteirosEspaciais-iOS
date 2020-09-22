f10db245c88727bb4384a0eb3f642b61aa103eaa
Patricia <patysiq@yahoo.com.br>
2020-09-22 08:40:51 -0300

    add files

--- EscoteiroEspacial/AppDelegate.swift
+++ EscoteiroEspacial/AppDelegate.swift
@@ -33,7 +33,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 //        completionHandler([.alert, .sound])
 //    }
     
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
