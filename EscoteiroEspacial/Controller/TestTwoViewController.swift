9d0ce8673c04177584873de1b3dc0dddd653bd6e
Kellyane Nogueira <kellyanenogueira1@gmail.com>
2020-09-23 17:57:24 -0300

    Adicionando snapBehavior no primeiro item

---
+++ EscoteiroEspacial/Controller/TestTwoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TestTwoViewController.swift
+//  EscoteiroEspacial
+//
+//  Created by Kellyane Nogueira on 22/09/20.
+//  Copyright © 2020 Mateus Nobre. All rights reserved.
+//
+
+import Foundation
