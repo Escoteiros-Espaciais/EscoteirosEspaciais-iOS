c0b8357ffced32175b4b73373fb6ead6192e1dfb
Kellyane Nogueira <kellyanenogueira1@gmail.com>
2020-09-24 14:34:53 -0300

    Generalizando as funções para todos os elementos

--- EscoteiroEspacial/Controller/TestTwoViewController.swift
+++ EscoteiroEspacial/Controller/TestTwoViewController.swift
@@ -23,7 +23,7 @@ class TestTwoViewController: UIViewController { //Mudar nome para QuestionTwo
     
     var dynamicAnimator: UIDynamicAnimator!
     var snapBehavior: UISnapBehavior!
-  
+
     override func viewDidLoad() {
         super.viewDidLoad()
         option1.image = UIImage(named: "Marte")
@@ -38,32 +38,36 @@ class TestTwoViewController: UIViewController { //Mudar nome para QuestionTwo
         dynamicAnimator = UIDynamicAnimator(referenceView: view)
         answer.isUserInteractionEnabled = false
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         snapBehaviorInImage(image: option1)
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pannedView))
-        option1.addGestureRecognizer(panGesture)
-        
-//        snapBehaviorInImage(image: option2)
-//        snapBehaviorInImage(image: option3)
-//        snapBehaviorInImage(image: option4)
-       
+        snapBehaviorInImage(image: option2)
+        snapBehaviorInImage(image: option3)
+        snapBehaviorInImage(image: option4)
     }
     
     func snapBehaviorInImage(image: UIImageView) {
         snapBehavior = UISnapBehavior(item: image, snapTo: image.center) //Change position
         image.isUserInteractionEnabled = true
         dynamicAnimator.addBehavior(snapBehavior)
+
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pannedView))
+        image.addGestureRecognizer(panGesture)
     }
     
     @objc func pannedView(recognizer: UIPanGestureRecognizer) {
+        var element = recognizer.view?.center ?? .zero
+        
         switch recognizer.state {
         case .began:
             dynamicAnimator.removeBehavior(snapBehavior)
         case .changed:
             let translation = recognizer.translation(in: view)
-            option1.center = CGPoint(x: option1.center.x + translation.x, y: option1.center.y + translation.y)
+            element = CGPoint(x: element.x + translation.x, y: element.y + translation.y)
             recognizer.setTranslation(.zero, in: view)
             
-            checkIntersectionWith(image: option1)
+            checkIntersectionWith(image: recognizer.view!)
         case .ended, .cancelled, .failed:
             dynamicAnimator.addBehavior(snapBehavior)
         case .possible:
@@ -74,8 +78,6 @@ class TestTwoViewController: UIViewController { //Mudar nome para QuestionTwo
     func checkIntersectionWith(image: UIView) {
         if answer.frame.contains(image.center) {
             snapBehavior.snapPoint = answer.center
-        } else {
-            snapBehavior.snapPoint = image.center
         }
     }
     
