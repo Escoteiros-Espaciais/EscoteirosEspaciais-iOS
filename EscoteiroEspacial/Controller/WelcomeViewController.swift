29c9e62bcc4d93e6ef08ec2c8f2221a55fc5e0f1
Paulo Rubem <rubem.oliveira27@hotmail.com>
2020-09-30 11:45:22 -0300

    Correcao tela de boas vindas

--- EscoteiroEspacial/Controller/WelcomeViewController.swift
+++ EscoteiroEspacial/Controller/WelcomeViewController.swift
@@ -15,7 +15,25 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+        if UserDefaults.standard.bool(forKey: "First Launch") == false{
+           
+            first()
+                    
+        } else {
+            noFirst()
+
+        }
+        UserDefaults.standard.set(true, forKey: "First Launch")
+    }
+    
+    func first() {
         label.text = "Olá viajante, hoje você está iniciando\nsua jornada pelo sistema solar!"
+    }
+    
+    func noFirst(){
+        label.text = "Vamos continuar\nnossa jornada pelo sistema solar!"
     }
   
     @IBAction func skipButton(_ sender: Any) {
