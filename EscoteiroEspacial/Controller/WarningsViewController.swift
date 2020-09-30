96d3afc6773a139b87197e7c61d1c1bd1e66d2ff
Patricia <patysiq@yahoo.com.br>
2020-09-30 11:44:32 -0300

    results

--- EscoteiroEspacial/Controller/WarningsViewController.swift
+++ EscoteiroEspacial/Controller/WarningsViewController.swift
@@ -21,9 +21,47 @@ class WarningsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print(astroId)
        // result = (100 * score)/5
         label.text = "Você acertou \(score)/5 das atividades 🎉"
 
     }
-
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "HomeSpaceController" {
+            guard segue.destination is HomeSpaceController else { return }
+        } else if segue.identifier == "InfoScreenController" {
+            guard let destVC = segue.destination as? InfoScreenController else { return }
+            destVC.astroIdentifier = segueStringToAstro(with: astroId)
+        }
+        
+    }
+    
+    // swiftlint:disable:next cyclomatic_complexity
+    func segueStringToAstro(with: String) -> Astro {
+        switch with {
+        case "sun":
+            return .sunn
+        case "mercury":
+            return .mercury
+        case "venus":
+            return .venus
+        case "moon":
+            return .moon
+        case "earth":
+            return .earth
+        case "mars":
+            return .mars
+        case "jupiter":
+            return .jupiter
+        case "saturn":
+            return .saturn
+        case "uran":
+            return .uran
+        case "neptun":
+            return .neptun
+        default:
+            return .unknown
+        }
+    }
+    
 }
